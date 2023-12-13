21c4bfd48653651064446d4cbfc1dbb5d7d7cd26
Marek Biros <marek.biros@carebot.com>
2023-12-13 11:04:16 +0100

    init

---
+++ variablesStudies/data.py
@@ -0,0 +1,101 @@
+import sys, os 
+from tqdm import tqdm
+import ROOT
+
+#hack to load from parent directory
+sys.path.append('../') 
+os.chdir("variablesStudies")
+
+from Naming.mcDict import mcDict
+from Naming.cuts import cuts_run1, var
+
+#turn off graphics mode
+ROOT.gROOT.SetBatch(True)
+
+path = "/eos/atlas/atlascerngroupdisk/phys-beauty/RKstarRun2/ntuples/muon_channel_v0.9/ntuples_updated/"
+path = "../data/"
+draw = True
+
+ntuple =  "data18_13TeV_periodK_part_03"
+def rename_htemp(name=""):
+    try:
+        h = ROOT.gPad.GetPrimitive("htemp")
+        h.SetTitle(name)
+    except:
+        pass
+
+range_ = {"BmumuKst_B_mass" : "BmumuKst_B_mass>5150 && BmumuKst_B_mass<5700",
+          "BmumuKst_Bbar_mass" :"BmumuKst_Bbar_mass>5150 && BmumuKst_Bbar_mass<5700" ,
+          "BmumuKst_kaonPion_mass" : "BmumuKst_kaonPion_mass > 846 && BmumuKst_kaonPion_mass < 946",
+          "BmumuKst_pionKaon_mass" :  "BmumuKst_pionKaon_mass > 846 && BmumuKst_pionKaon_mass < 946",
+          "BmumuKst_diMuon_mass*BmumuKst_diMuon_mass" : "BmumuKst_diMuon_mass*BmumuKst_diMuon_mass>1000000 &&  BmumuKst_diMuon_mass*BmumuKst_diMuon_mass < 8000000",
+          f"{var['tau']}/{var['tau']}_err" : f"{var['tau']}/{var['tau']}_err>0 && {var['tau']}/{var['tau']}_err<30",
+          "BmumuKst_chi2_over_nDoF" : "BmumuKst_chi2_over_nDoF<3",
+          "BmumuKst_diMuon_chi2_over_nDoF" : "BmumuKst_diMuon_chi2_over_nDoF>0",
+          "BmumuKst_muon0_pT": "BmumuKst_muon0_pT<20000",
+          "BmumuKst_muon1_pT": "BmumuKst_muon1_pT<20000",
+          "BmumuKst_diMuon_pT": "BmumuKst_diMuon_pT<50000"
+         }
+
+rows = len(range_)
+c = ROOT.TCanvas("c","c",3*1024,rows*1024)
+c.Divide(3,rows)
+
+file_name = os.path.join(path,"ntuple-"+ntuple+".root")
+f = ROOT.TFile(file_name)
+t = f.Get("Nominal/BaseSelection_KStarMuMu_BmumuKstSelection")
+
+N_events = t.Draw("1","","goff")
+
+N_all = t.Draw("1","BmumuKst_B_mass>0","goff")
+N_passed = t.Draw("1",cuts_run1,"goff")
+
+# N_cut = t.Draw("1",cut,"goff")
+# N_cut_passed = t.Draw("1",cut+"&&"+cuts_run1,"goff")
+
+print(N_events, N_all, N_passed)
+# print(N_cut,N_cut_passed)
+
+if draw:
+    for j,v in enumerate(tqdm(range_)):
+        c.cd(1+3*j)
+        
+        t.Draw(v)
+        rename_htemp(v.replace("BmumuKst_",""))
+        
+        c.cd(2+3*j)
+        t.Draw(v,range_[v])
+        rename_htemp("zoom")
+            
+        c.cd(3+3*j)
+        t.Draw(v,range_[v]+" && "+cuts_run1)
+        rename_htemp("RUN 1 cuts")    
+
+    c.Draw()
+    c.SaveAs(f"out/{ntuple}.png")
+    c.SaveAs(f"out/{ntuple}.pdf")
+        
+ 
+
+    # with open(f"out/{decay}_{cutFlag}.log", 'w') as f:
+    #     f.write(f"{cutFlag} {cut} \n")
+    #     f.write(f"events: {N_events}\n")
+    #     f.write(f"all: {N_all}, passed run1 cuts: {N_passed}\n")
+    #     f.write(f"selected: {N_cut}, passed run1 cuts: {N_cut_passed}\n")
+    #     f.write(f"selected over events: {N_cut/N_events}, passed run1 cuts: {N_cut_passed/N_events}\n")
+    #     f.write("\n")
+    #     f.write("plotted vars in range:\n")
+    #     for r in range_:
+    #         f.write(f"{r} - {range_[r]} \n")
+    #     f.write("\n")
+    #     f.write("\n")
+    #     f.write(f"{cuts_run1}\n")
+
+    # with open(f"out/results.log", 'w') as f:
+    #     f.write(f"{cutFlag} {cut} \n")
+    #     f.write(f"events: {N_events}\n")
+    #     f.write(f"all: {N_all}, passed run1 cuts: {N_passed}\n")
+    #     f.write(f"selected: {N_cut}, passed run1 cuts: {N_cut_passed}\n")
+    #     f.write(f"selected over events: {N_cut/N_events}, passed run1 cuts: {N_cut_passed/N_events}\n")
+    #     f.write("\n")
+        
---
+++ variablesStudies/misReconstruct.py
@@ -0,0 +1,120 @@
+import sys, os
+from ROOT import *
+import ROOT
+import numpy as np
+import uproot3 as uproot
+import uproot3_methods as um
+import collections
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from itertools import chain
+from tqdm import tqdm
+
+#hack to load from parent directory
+sys.path.append('../') 
+# os.chdir("massFit")
+from Naming.mcDict import mcDict
+from Naming.varDict import varDict
+from Naming.cuts import cuts_run1, var
+from Helpers.timeMeasuring import timer
+from Fitting.mass.Bfunctions import *
+from Fitting.mass.Kfunctions import *
+
+gROOT.SetBatch(True)
+
+# Load the header and implementation files
+gROOT.ProcessLine('.L ../fitCore/MyPlot.h')
+gROOT.ProcessLine('.L ../fitCore/MyPlot.cxx')
+
+# Declare the class in PyROOT
+gROOT.ProcessLine("class MyPlot;")
+
+mc_dict = mcDict(debugPrint=False)
+var_dict = varDict()
+
+# ch = TChain("Nominal/BaseSelection_KStarMuMu_BmumuKstSelection", "Nominal/BaseSelection_KStarMuMu_BmumuKstSelection")
+ch = TChain("BdBestChi")
+ch.Add(f"../data/ntuple-{mc_dict['Bd_Kstar_mumu']}_part_combi12_bestCand.root")
+# ch.Add(f"../data/ntuple-{mc_dict['Bdbar_Kstar_mumu']}_part_01.root")
+# ch.Add(f"../data/ntuple-data18_13TeV_periodK_part_03.root")
+
+
+ks = var_dict["Kstar_mass"]
+ksb = var_dict["Kstarb_mass"]
+cut = "BmumuKst_isTrueNonresBd==1"
+
+sig_range = (846, 946)
+pdg = 896
+# h_ks = TH1F("h_ks", "Kstar_mass Histogram", 100, sig_range[0], sig_range[1])
+# h_ksb = TH1F("h_ksb", "Kstar_bar_mass Histogram", 100, sig_range[0], sig_range[1])
+
+# ch.Draw(ks+">>h_ks", cut+"&&"+ks+">"+str(sig_range[0])+"&&"+ks+"<"+str(sig_range[1]))
+# ch.Draw(ksb+">>h_ksb", cut+"&&"+ksb+">"+str(sig_range[0])+"&&"+ksb+"<"+str(sig_range[1]))
+
+# h_ks.Scale(1.0/h_ks.Integral())
+# h_ksb.Scale(1.0/h_ksb.Integral())
+# h_ksb.SetLineColor(kRed)
+
+# c.cd(1)
+# h_ks.Draw()
+# h_ksb.Draw("same")
+
+# c.cd(2)
+range = (-90, 90)
+
+ks = var_dict["Kstar_mass"]
+ksb = var_dict["Kstarb_mass"]
+cut = "BmumuKst_isTrueNonresBd==1"
+
+hksp = TH1F("h_ksp", "Kstar_mass Histogram", 100, 0, range[1])
+hksm = TH1F("h_ksm", "Kstar_mass Histogram", 100, range[0], 0)
+hksbp = TH1F("h_ksbp", "Kstar_mass Histogram", 100, 0, range[1])
+hksbm = TH1F("h_ksbm", "Kstar_mass Histogram", 100, range[0], 0)
+# h_ksb = TH1F("h_ksb", "Kstar_bar_mass Histogram", 100, range[0], range[1])
+
+ch.Draw(str(pdg)+"-"+ks +">>h_ksp", str(pdg)+"-"+ks +">0 &&"+cut +"&&"+ks+">"+str(sig_range[0])+"&&"+ks+"<"+str(sig_range[1])+"&&"+ksb+">"+str(sig_range[0])+"&&"+ksb+"<"+str(sig_range[1])) #   +">>h_diff"
+ch.Draw(str(pdg)+"-"+ks +">>h_ksm", str(pdg)+"-"+ks +"<0 &&"+cut +"&&"+ks+">"+str(sig_range[0])+"&&"+ks+"<"+str(sig_range[1])+"&&"+ksb+">"+str(sig_range[0])+"&&"+ksb+"<"+str(sig_range[1])) #   +">>h_diff"
+ch.Draw(str(pdg)+"-"+ksb +">>h_ksbp", str(pdg)+"-"+ksb +">0 &&"+cut +"&&"+ks+">"+str(sig_range[0])+"&&"+ks+"<"+str(sig_range[1])+"&&"+ksb+">"+str(sig_range[0])+"&&"+ksb+"<"+str(sig_range[1])) #   +">>h_diff"
+ch.Draw(str(pdg)+"-"+ksb +">>h_ksbm", str(pdg)+"-"+ksb +"<0 &&"+cut +"&&"+ks+">"+str(sig_range[0])+"&&"+ks+"<"+str(sig_range[1])+"&&"+ksb+">"+str(sig_range[0])+"&&"+ksb+"<"+str(sig_range[1])) #   +">>h_diff"
+# ch.Draw(ksb+">>h_ksb", cut+"&&"+ksb+">"+str(range[0])+"&&"+ksb+"<"+str(range[1]))
+
+c = TCanvas("c", "c", 1024,1024)
+c.Divide(2,2)
+c.cd(1)
+hksm.Draw()
+c.cd(2)
+hksp.Draw()
+c.cd(3)
+hksbm.Draw()
+c.cd(4)
+hksbp.Draw()
+
+# h_ks.Scale(1.0/h_ks.Integral())
+# h_ksb.Scale(1.0/h_ksb.Integral())
+# h_ksb.SetLineColor(kRed)
+
+
+
+
+# range = (0, 100)
+# ks = var_dict["Kstar_mass"]+"_err"
+# ksb = var_dict["Kstarb_mass"]+"_err"
+
+# h_ks = TH1F("h_ks_err", "Kstar_mass Histogram", 100, range[0], range[1])
+# h_ksb = TH1F("h_ksb_err", "Kstar_bar_mass Histogram", 100, range[0], range[1])
+
+# ch.Draw(ks+">>h_ks_err", cut+"&&"+ks+">"+str(range[0])+"&&"+ks+"<"+str(range[1]))
+# ch.Draw(ksb+">>h_ksb_err", cut+"&&"+ksb+">"+str(range[0])+"&&"+ksb+"<"+str(range[1]))
+
+# h_ks.Scale(1.0/h_ks.Integral())
+# h_ksb.Scale(1.0/h_ksb.Integral())
+# h_ksb.SetLineColor(kRed)
+
+# c.cd(1)
+# ch.Draw(ks+"_err","BmumuKst_isTrueNonresBd==1")
+# c.cd(1)
+# ch.Draw(ks+"_err","BmumuKst_isTrueNonresBd==1")
+
+
+c.SaveAs("./out/kstar.pdf")
+# ch.Draw(var_dict['B_mass'],"BmumuKst_isTrueNonresBd==0")
