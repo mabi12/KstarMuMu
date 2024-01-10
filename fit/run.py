4eb7f60f030b889286a7ed35a5754ced5e03b029
Marek Biros <marek.biros@carebot.com>
2024-01-10 13:09:33 +0100

    renaming, adding argparser

--- fit/run.py
+++ fit/run.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from itertools import chain
 from tqdm import tqdm
+import argparse
 
 #hack to load from parent directory
 sys.path.append('../') 
@@ -22,13 +23,16 @@ from Fitting.Mass.Bfunctions import *
 from Fitting.Mass.Kfunctions import *
 from Fitting.Angles.Afunctions import *
 
+parser = argparse.ArgumentParser(description='Process input parameters for the ML fit.')
+parser.add_argument('fit_functions', default="john_voig_p333", help='fitfunctions for Bmass_Kstmass_angles, s for spherical harmonics, p for product of polynomials, numbers for the order of polynomial for cos theta_K, cos theta_L, phi respectively')
+args = parser.parse_args()
+
+#Do not create plotting windows
 gROOT.SetBatch(True)
 
-# Load the header and implementation files
+# Load the header and implementation files and Declare the class in PyROOT
 gROOT.ProcessLine('.L ../fitCore/MyPlot.h')
 gROOT.ProcessLine('.L ../fitCore/MyPlot.cxx')
-
-# Declare the class in PyROOT
 gROOT.ProcessLine("class MyPlot;")
 
 csv_file_path = "./out/fit_results.csv"
@@ -46,8 +50,10 @@ data = RooDataSet("data", "My RooDataSet", ch, variables)
 
 
 
+func = fit_functions.split("_")
 angularPdf = RooProdPdf("angularPdf", "angularPdf", RooArgList(poly_theta_K, poly_theta_L, poly_phi ))
-prodPdf = RooProdPdf("prodPdf", "prodPdf", RooArgList(johnson, K_mass_fit['vfix'], angularPdf))
+
+prodPdf = RooProdPdf("prodPdf", "prodPdf", RooArgList(B_mass_fit[func[0]], K_mass_fit[func[1]], angularPdf))
 # [3,3,3,4,2]):
 
 fit_result = prodPdf.fitTo(data, RooFit.Save())
